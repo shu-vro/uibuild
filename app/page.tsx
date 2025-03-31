f6caac129f1183691c46afdcbdafa0126407b099
shu-vro <official.shirshen@gmail.com>
2025-03-31 21:06:03 +0600

    chore: signup form done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { Metadata } from "next";
 import React from "react";
 import HomePage from "./components/HomePage";
+import { createMetadata } from "@/lib/metadata";
 
-export const metadata: Metadata = {
-    title: "Editor",
-    description: "Editor for UiBuild",
-};
+const APP_NAME = "Editor";
+const APP_DEFAULT_TITLE = "UiBuild";
+const APP_TITLE_TEMPLATE = "%s • UiBuild";
+const APP_DESCRIPTION = "Best app in the world!";
+
+export const metadata: Metadata = createMetadata({
+    appName: APP_NAME,
+    description: APP_DESCRIPTION,
+});
 
 export default function page() {
     return <HomePage />;
--- components/ui/sheet.tsx
+++ components/ui/sheet.tsx
@@ -22,7 +22,7 @@ const SheetOverlay = React.forwardRef<
     <SheetPrimitive.Overlay
         className={cn(
             "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-            className
+            className,
         )}
         {...props}
         ref={ref}
@@ -44,7 +44,7 @@ const sheetVariants = cva(
         defaultVariants: {
             side: "right",
         },
-    }
+    },
 );
 
 interface SheetContentProps
@@ -60,7 +60,8 @@ const SheetContent = React.forwardRef<
         <SheetPrimitive.Content
             ref={ref}
             className={cn(sheetVariants({ side }), className)}
-            {...props}>
+            {...props}
+        >
             {children}
             <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
                 <X className="h-4 w-4" />
@@ -75,7 +76,7 @@ const SheetHeader = ({ className, ...props }: React.ComponentProps<"div">) => (
     <div
         className={cn(
             "flex flex-col space-y-2 text-center sm:text-left",
-            className
+            className,
         )}
         {...props}
     />
@@ -86,7 +87,7 @@ const SheetFooter = ({ className, ...props }: React.ComponentProps<"div">) => (
     <div
         className={cn(
             "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-            className
+            className,
         )}
         {...props}
     />
